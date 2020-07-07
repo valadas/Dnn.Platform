be3aa439ab12700e63a564107f75ea326838960c
Daniel Valadas <info@danielvaladas.com>
2020-07-07 04:19:26 -0400

    Improved logic of inline svg support

--- DNN Platform/Website/admin/Skins/Logo.ascx.cs
+++ DNN Platform/Website/admin/Skins/Logo.ascx.cs
@@ -4,40 +4,52 @@
 namespace DotNetNuke.UI.Skins.Controls
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.UI.WebControls;
     using System.Xml.Linq;
+
     using DotNetNuke.Abstractions;
     using DotNetNuke.Common;
     using DotNetNuke.Common.Utilities;
-    using DotNetNuke.Entities.Host;
     using DotNetNuke.Services.Cache;
     using DotNetNuke.Services.Exceptions;
     using DotNetNuke.Services.FileSystem;
     using Microsoft.Extensions.DependencyInjection;
 
-    /// -----------------------------------------------------------------------------
-    /// <summary></summary>
-    /// <returns></returns>
-    /// <remarks></remarks>
-    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// Logo SkinObject.
+    /// </summary>
     public partial class Logo : SkinObjectBase
     {
-        private readonly INavigationManager _navigationManager;
-        private readonly string _cacheKey;
+        private readonly INavigationManager navigationManager;
+        private readonly string svgCacheKey;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logo"/> class.
+        /// </summary>
         public Logo()
         {
-            this._navigationManager = Globals.DependencyProvider.GetRequiredService<INavigationManager>();
-            this._cacheKey = string.Format(DataCache.PortalCacheKey, this.PortalSettings.PortalId, this.PortalSettings.CultureCode) + "LogoSvg";
+            this.navigationManager = Globals.DependencyProvider.GetRequiredService<INavigationManager>();
+            this.svgCacheKey = string.Format(DataCache.PortalCacheKey, this.PortalSettings.PortalId, this.PortalSettings.CultureCode) + "LogoSvg";
         }
 
+        /// <summary>
+        /// Gets or sets the width of the border around the image.
+        /// </summary>
         public string BorderWidth { get; set; }
 
+        /// <summary>
+        /// Gets or sets the css class for the image.
+        /// </summary>
         public string CssClass { get; set; }
 
-        public Nullable<bool> InjectSVG { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating wether to inject the svg content inline instead or wrapping it in an img tag.
+        /// </summary>
+        public bool? InjectSVG { get; set; }
 
+        /// <inheritdoc/>
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -47,44 +59,74 @@ namespace DotNetNuke.UI.Skins.Controls
 
                 if (!string.IsNullOrEmpty(this.PortalSettings.LogoFile))
                 {
-                    this.litLogo.Text = this.PortalSettings.PortalName; // default valuee is case we can't load an image or svg
+                    // default value in case we can't load an image or svg
+                    this.litLogo.Text = this.PortalSettings.PortalName;
 
                     var fileInfo = this.GetLogoFileInfo();
                     if (fileInfo != null)
                     {
-                        if (this.InjectSVG.GetValueOrDefault() == true) // try to load as <svg>
+                        if (this.InjectSVG.GetValueOrDefault() && fileInfo.Extension == "svg")
                         {
+                            // svg injection was requested and we have an svg file.
                             this.imgLogo.Visible = false;
-                            this.hypLogo.CssClass = this.CssClass;
-
-                            string svg = (string)CachingProvider.Instance().GetItem(this._cacheKey);
+                            string svg = DataCache.GetCache<string>(this.svgCacheKey);
+                            var svgXmlDoc = new XDocument();
                             if (string.IsNullOrEmpty(svg))
                             {
-                                if (fileInfo.Extension == "svg")
+                                using (var fileContent = FileManager.Instance.GetFileContent(fileInfo))
                                 {
-                                    try
-                                    {
-                                        XDocument svgXmlDoc = XDocument.Load(fileInfo.PhysicalPath);
-                                        XElement svgXmlNode = svgXmlDoc.Descendants().Where(x => x.Name.LocalName == "svg").SingleOrDefault();
-
-                                        if (svgXmlNode != null)
-                                        {
-                                            svg = svgXmlNode.ToString();
-                                            CachingProvider.Instance().Insert(this._cacheKey, svg);
-                                        }
-                                    }
-                                    catch (Exception ex)
-                                    { }
+                                    svgXmlDoc = XDocument.Load(fileContent);
                                 }
+
+                                var svgXmlNode = svgXmlDoc.Descendants().Where(x => x.Name.LocalName == "svg").SingleOrDefault();
+
+                                if (svgXmlNode == null)
+                                {
+                                    throw new InvalidFileContentException("Invalid svg file.");
+                                }
+
+                                var ns = svgXmlNode.GetDefaultNamespace();
+
+                                if (!string.IsNullOrEmpty(this.CssClass))
+                                {
+                                    // Append the css class.
+                                    List<string> classList = new List<string>();
+                                    classList.Add(this.CssClass);
+
+                                    if (svgXmlNode.Attribute("class") != null)
+                                    {
+                                        classList.AddRange(svgXmlNode.Attribute("class").Value.Split(' '));
+                                    }
+
+                                    svgXmlNode.SetAttributeValue("class", string.Join(" ", classList.ToArray()));
+                                }
+
+                                if (svgXmlNode.Descendants().FirstOrDefault(x => x.Name.LocalName == "title") == null)
+                                {
+                                    // Add the title for ADA compliance.
+                                    var titleNode = new XElement(
+                                        ns + "title",
+                                        new XAttribute("id", this.litLogo.UniqueID),
+                                        this.PortalSettings.PortalName);
+
+                                    svgXmlNode.AddFirst(new XElement(titleNode));
+
+                                    // Link the title to the svg node.
+                                    svgXmlNode.SetAttributeValue("aria-labelledby", this.litLogo.UniqueID);
+                                }
+
+                                // Ensure we have the image role for ADA Compliance
+                                svgXmlNode.SetAttributeValue("role", "img");
+
+                                svg = svgXmlNode.ToString();
+                                DataCache.SetCache(this.svgCacheKey, svg);
                             }
 
-                            if (!string.IsNullOrEmpty(svg))
-                            {
-                                this.litLogo.Text = svg;
-                            }
+                            this.litLogo.Text = svg;
                         }
-                        else // try to load as <img>
+                        else
                         {
+                            // display the raster image
                             this.imgLogo.Visible = true;
 
                             string imageUrl = FileManager.Instance.GetUrl(fileInfo);
@@ -113,13 +155,12 @@ namespace DotNetNuke.UI.Skins.Controls
                 this.hypLogo.Attributes.Add("aria-label", this.PortalSettings.PortalName);
                 if (this.PortalSettings.HomeTabId != -1)
                 {
-                    this.hypLogo.NavigateUrl = this._navigationManager.NavigateURL(this.PortalSettings.HomeTabId);
+                    this.hypLogo.NavigateUrl = this.navigationManager.NavigateURL(this.PortalSettings.HomeTabId);
                 }
                 else
                 {
                     this.hypLogo.NavigateUrl = Globals.AddHTTP(this.PortalSettings.PortalAlias.HTTPAlias);
                 }
-
             }
             catch (Exception exc)
             {
