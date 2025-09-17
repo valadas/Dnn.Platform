de99ad474ad65391c33c076d4f9610d3ff301f25
Daniel <info@danielvaladas.com>
2025-09-17 17:06:41 -0400

    Began Implementation of a new Jodit HTML Editor Provider

---
+++ DNN Platform/Providers/HtmlEditorProviders/Dnn.Jodit/EditorControl.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dnn.JoditEditorProvider
+{
+    public class EditorControl : WebControl
+    {
+        public EditorControl()
+        {
+            // TODO: IMPLEMENT THIS PROPERLY
+            this.Height = Unit.Pixel(300);
+            this.Width = Unit.Percentage(100);
+        }
+
+        [DefaultValue("")]
+        public string Value
+        {
+            get
+            {
+                var value = this.ViewState["Value"];
+                return value == null ? string.Empty : (string)value;
+            }
+
+            set
+            {
+                this.ViewState["Value"] = value;
+            }
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            writer.WriteLine("<h1>Custom Editor is loading!!!</h1>");
+        }
+    }
+}
---
+++ DNN Platform/Providers/HtmlEditorProviders/Dnn.Jodit/JoditHtmlEditorProvider.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetNuke.Common;
+using DotNetNuke.Modules.HTMLEditorProvider;
+
+namespace Dnn.JoditEditorProvider
+{
+    /// <summary>
+    /// Implementes DNN HtmlEditorProvider for Jodit Editor.
+    /// </summary>
+    public class JoditHtmlEditorProvider : HtmlEditorProvider
+    {
+        private EditorControl htmlEditorControl;
+        private string rootImageDirectory;
+
+        public override Control HtmlEditorControl => this.htmlEditorControl;
+
+        public override ArrayList AdditionalToolbars { get; set; }
+
+        public override string ControlID { get; set; }
+
+        public override string RootImageDirectory
+        {
+            get
+            {
+                if (this.rootImageDirectory == string.Empty)
+                {
+                    // Remove the Application Path from the Home Directory
+                    return Globals.ApplicationPath != string.Empty
+                               ? this.PortalSettings.HomeDirectory.Replace(Globals.ApplicationPath, string.Empty)
+                               : this.PortalSettings.HomeDirectory;
+                }
+
+                return this.rootImageDirectory;
+            }
+
+            set
+            {
+                this.rootImageDirectory = value;
+            }
+        }
+
+        public override string Text
+        {
+            get => this.htmlEditorControl.Value;
+            set => this.htmlEditorControl.Value = value; 
+        }
+
+        public override Unit Width
+        {
+            get => this.htmlEditorControl.Width;
+            set => this.htmlEditorControl.Width = value; 
+        }
+
+        public override Unit Height
+        {
+            get => this.htmlEditorControl.Height;
+            set => this.htmlEditorControl.Height = value; 
+        }
+
+        public override void AddToolbar()
+        {
+        }
+
+        public override void Initialize()
+        {
+            this.htmlEditorControl = new EditorControl { ID = this.ControlID };
+        }
+    }
+}
